8d6cf0ea6211ca04ff07e0e42826b73b7d89c3ce
Surendra Singh Gangwar <sgangwar00@gmail.com>
2025-12-05 10:43:03 +0530

    a
    aaaaaaaaaaa

--- ACUtilities/src/main/java/base/BaseClass.java
+++ ACUtilities/src/main/java/base/BaseClass.java
@@ -80,7 +80,7 @@ public class BaseClass {
         driver.get(url);
         LOGGER.info("Browser " + browser + " launched and navigated to " + url);         
         
-        driver.findElement(By.xpath("//*[@id=\"login_link_top\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"login_link_top\"]")).click();                                                                                                             
         driver.findElement(By.id("Bugzilla_login_top")).sendKeys(username);             
         driver.findElement(By.id("Bugzilla_password_top")).sendKeys(password);
 
